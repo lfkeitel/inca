ba342cc143546eff65bb799b146b1533111040b0
Lee Keitel <keitellf@gmail.com>
2015-01-12 19:45:34 -0600

    Updated install documentation, create icauser in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,18 +5,21 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo ">>>>> Installing TFTP server and Expect <<<<<"
+echo ">> Installing TFTP server and Expect"
 apt-get update
 apt-get install -y xinetd tftpd tftp tar expect
-echo ">>>>> Creating service <<<<<"
+echo ">>>> Creating service"
 cat ./service-tftp.conf > /etc/xinetd.d/tftp
-echo ">>>>> Setting up directory <<<<<"
+echo ">>>> Setting up directory"
 useradd -r -s /bin/false tftpuser
 mkdir /tftpboot
 chmod -R 777 /tftpboot
 chown -R nobody /tftpboot
-echo ">>>>> Starting server <<<<<"
+echo ">>>> Starting server"
 /etc/init.d/xinetd restart
-echo ">>>>> TFTP is now installed <<<<<"
+echo ">>>> TFTP is now installed"
 echo ""
+echo ">> Creating icauser"
+useradd icauser
+echo ">>>> User created"
 exit
