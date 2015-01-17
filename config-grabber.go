aaa9ac24077622281d819b1d0efde58aa7b0ab28
Lee Keitel <keitellf@gmail.com>
2015-01-16 19:35:17 -0600

    Moved configuration files to common directory

--- config-grabber.go
+++ config-grabber.go
@@ -3,17 +3,17 @@ package main
 import (
 	"github.com/BurntSushi/toml"
 
-	"github.com/dragonrider23/infrastructure-config-archive/server"
-	"github.com/dragonrider23/infrastructure-config-archive/interfaces"
-    "github.com/dragonrider23/infrastructure-config-archive/grabber"
 	logger "github.com/dragonrider23/go-logger"
+	"github.com/dragonrider23/infrastructure-config-archive/grabber"
+	"github.com/dragonrider23/infrastructure-config-archive/interfaces"
+	"github.com/dragonrider23/infrastructure-config-archive/server"
 )
 
 var appLogger = logger.New("app").Verbose(3)
 
 func loadAppConfig() (interfaces.Config, error) {
 	var conf interfaces.Config
-	if _, err := toml.DecodeFile("configuration.toml", &conf); err != nil {
+	if _, err := toml.DecodeFile("config/configuration.toml", &conf); err != nil {
 		appLogger.Fatal("Couldn't load configuration: %s", err.Error())
 		return interfaces.Config{}, err
 	}
